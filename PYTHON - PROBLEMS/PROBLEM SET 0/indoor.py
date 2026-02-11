be36ea1833db1dad3bdd75a961014d7bb4eac117
Evycostzocn <profissionalevelyncosta@gmail.com>
2026-02-11 15:37:02 -0300

    feat: Solução do primeiro problema da semana 0

---
+++ PYTHON - PROBLEMS/PROBLEM SET 0/indoor.py
@@ -0,0 +1,8 @@
+""" 
+Implement a program in Python that prompts the user for input and then outputs that same input in lowercase. Punctuation and whitespace should be outputted unchanged.
+"""
+
+word = input()
+
+word = word.lower()
+print(word)
