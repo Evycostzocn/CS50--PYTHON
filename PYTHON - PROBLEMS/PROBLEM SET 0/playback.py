5848c7b727c287eb3ef75de3ad39444b41727b14
Evycostzocn <profissionalevelyncosta@gmail.com>
2026-02-23 14:13:59 -0300

    Solução do segundo problema da semama 0

---
+++ PYTHON - PROBLEMS/PROBLEM SET 0/playback.py
@@ -0,0 +1,8 @@
+"""
+implement a program in Python that prompts the user for input and then outputs that same input, replacing each space with ... (i.e., three periods).
+"""
+
+text = input()
+
+text = text.replace(" ", "...")
+print(text)
